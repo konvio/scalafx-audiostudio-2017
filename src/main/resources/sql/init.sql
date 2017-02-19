e868e0d3e974f968ce47e4e3a1cf1c97b660ab59
konv <konvdev@gmail.com>
2017-02-19 13:12:31 +0200

    Add database init script

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE artist (
+  id              SERIAL PRIMARY KEY,
+  name            TEXT NOT NULL,
+  registered_date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE record (
+  id            SERIAL PRIMARY KEY,
+  title         TEXT NOT NULL,
+  artist_id     INTEGER REFERENCES artist,
+  price         NUMERIC DEFAULT 0,
+  released_date DATE    DEFAULT CURRENT_DATE,
+  path          TEXT
+);
+
+CREATE TABLE album (
+  id            SERIAL PRIMARY KEY,
+  title         TEXT NOT NULL,
+  price         NUMERIC DEFAULT 0,
+  released_date DATE    DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE album_record (
+  id           SERIAL PRIMARY KEY,
+  record_id    INTEGER REFERENCES record,
+  album_id     INTEGER REFERENCES album,
+  track_number INTEGER DEFAULT 0,
+  UNIQUE (record_id, album_id)
+);
+
