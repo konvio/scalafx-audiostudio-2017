8917962dcbaad2c52917db8c97e4acbc36db6734
konv <konvdev@gmail.com>
2017-02-27 19:34:36 +0200

    Incapsulate dialogs

--- src/main/scala/io/konv/audiostudio/Dialogs.scala
+++ src/main/scala/io/konv/audiostudio/Dialogs.scala
@@ -1,5 +1,13 @@
 package io.konv.audiostudio
 
+import scalafx.scene.control.{Dialog, TextInputDialog}
+
 object Dialogs {
+
+  val addArtist = new TextInputDialog() {
+    title = "Audio Studio"
+    headerText = "Add Artist"
+    contentText = "Name"
+  }
 
 }
--- src/main/scala/io/konv/audiostudio/controller/MainController.scala
+++ src/main/scala/io/konv/audiostudio/controller/MainController.scala
@@ -3,17 +3,16 @@ package io.konv.audiostudio.controller
 import java.sql.Date
 import javafx.collections.FXCollections
 
-import io.konv.audiostudio.DBManager
+import io.konv.audiostudio.Includes._
+import io.konv.audiostudio.{DBManager, Dialogs}
+import slick.jdbc.GetResult
 import slick.jdbc.PostgresProfile.api._
 
-import scalafx.Includes._
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
-import scalafx.scene.control.{TableColumn, TableView, TextInputDialog}
+import scalafx.scene.control.{TableColumn, TableView}
 import scalafxml.core.macros.sfxml
-import io.konv.audiostudio.Includes._
-import slick.jdbc.GetResult
 
 case class Artist(id: Int, name: String, date: Date)
 
@@ -29,7 +28,7 @@ class MainController(val tableView: TableView[Artist],
   private val items = tableView.items.get()
 
   initTable()
-  populateData()
+  update()
 
   private def initTable(): Unit = {
     id.cellValueFactory = v => v.value.id.toString
@@ -37,7 +36,7 @@ class MainController(val tableView: TableView[Artist],
     date.cellValueFactory = v => v.value.date.toString
   }
 
-  private def populateData(): Unit = {
+  private def update(): Unit = {
     val query = sql"SELECT id, name, registered_date FROM artist".as[Artist]
     db.run(query).onComplete {
       case Success(v) => tableView.items.set(FXCollections.observableList(v.asJava))
@@ -46,21 +45,12 @@ class MainController(val tableView: TableView[Artist],
   }
 
   def addArtist(): Unit = {
-    val dialog = new TextInputDialog() {
-      initOwner(tableView.scene.value.getWindow)
-      title = "Title"
-      headerText = "Header"
-      contentText = "Content"
-    }
-
-    val result = dialog.showAndWait()
-
+    val result = Dialogs.addArtist.showAndWait()
     result match {
-      case Some(name) => db.run(sqlu"INSERT INTO artist(name) VALUES ('#$name')").onComplete {
-        case Success(v) => populateData()
+      case Some(v) => db.run(sqlu"INSERT INTO artist(name) VALUES ('#$v')").onComplete {
+        case Success(v) => update()
       }
     }
-
   }
 
 }
