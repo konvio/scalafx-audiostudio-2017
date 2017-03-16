6e8930f02a159ca24c2d7834109401e70beeb9c1
konv <konvdev@gmail.com>
2017-03-16 14:13:32 +0200

    Add genre query

--- src/main/scala/io/konv/audiostudio/DBManager.scala
+++ src/main/scala/io/konv/audiostudio/DBManager.scala
@@ -1,6 +1,6 @@
 package io.konv.audiostudio
 
-import io.konv.audiostudio.models.Artist
+import io.konv.audiostudio.models.{Artist, Genre}
 import slick.jdbc.GetResult
 import slick.jdbc.PostgresProfile.api._
 
@@ -12,7 +12,13 @@ object DBManager {
 
   def artists(): Future[Vector[Artist]] = {
     implicit val getResult = GetResult[Artist](r => Artist(r.<<, r.<<, r.<<))
-    val query = sql"SELECT id, name, registered_date FROM artistChoiceBox".as[Artist]
+    val query = sql"SELECT id, name, registered_date FROM artist".as[Artist]
+    database.run(query)
+  }
+
+  def genres(): Future[Vector[Genre]] = {
+    implicit val getGenre = GetResult[Genre](r => Genre(r.<<, r.<<, r.<<))
+    val query = sql"SELECT id, name, description FROM genre".as[Genre]
     database.run(query)
   }
 
