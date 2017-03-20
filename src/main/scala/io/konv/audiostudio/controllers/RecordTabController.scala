ec45bed0768de68d17551aab2526ca023f08953d
konv <konvdev@gmail.com>
2017-03-20 14:45:23 +0200

    Add path column

--- src/main/scala/io/konv/audiostudio/controllers/RecordTabController.scala
+++ src/main/scala/io/konv/audiostudio/controllers/RecordTabController.scala
@@ -15,29 +15,32 @@ import scala.util.{Failure, Success}
 import scalafx.scene.control.{TableColumn, TableView}
 import scalafxml.core.macros.sfxml
 
-case class RecordTab(id: Int, title: String, price: Int, date: Date, artist: String)
+case class RecordTab(id: Int, title: String, price: Int, date: Date, path: String, artist: String)
 
 trait RecordTabTrait {
   def update(): Unit
 }
+
 @sfxml
 class RecordTabController(table: TableView[RecordTab],
                           title: TableColumn[RecordTab, String],
                           artist: TableColumn[RecordTab, String],
                           price: TableColumn[RecordTab, String],
-                          date: TableColumn[RecordTab, String]
+                          date: TableColumn[RecordTab, String],
+                          path: TableColumn[RecordTab, String]
                          ) extends RecordTabTrait {
 
   title.cellValueFactory = v => v.value.title
   artist.cellValueFactory = v => v.value.artist
   price.cellValueFactory = v => v.value.price.toString
   date.cellValueFactory = v => v.value.date.toString
+  path.cellValueFactory = v => v.value.path
   update()
 
   override def update(): Unit = {
-    implicit val getResult = GetResult(r => RecordTab(r.<<, r.<<, r.<<, r.<<, r.<<))
+    implicit val getResult = GetResult(r => RecordTab(r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
     val query = sql"""
-                  SELECT record.id, record.title, record.price, record.released_date, artist.name
+                  SELECT record.id, record.title, record.price, record.released_date, record.path, artist.name
                   FROM record INNER JOIN artist ON record.artist_id = artist.id
       """.as[RecordTab]
     DBManager.db.run(query).onComplete {
